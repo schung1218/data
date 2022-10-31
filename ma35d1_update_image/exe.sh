e395a22edbeb6e11f85737541dae5942335c757c
schung <schung@nuvoton.com>
2022-10-31 08:32:41 +0800

    Update ma35d1_update_image

---
+++ ma35d1_update_image/exe.sh
@@ -0,0 +1,7 @@
+make LOCALVERSION= -j8
+#git diff > modify.patch
+#cp modify.patch /home/schung/work/ma35d1/ma35d1-yocto/sources/meta-ma35d1/recipes-kernel/linux/files/
+
+
+# make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LOCALVERSION= -j8
+
